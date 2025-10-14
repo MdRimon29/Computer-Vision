57ab56254f988bd7017a850382e511ae6e9e3ef6
MdRimon29 <mdraisul.i.rimon@gmail.com>
2025-10-14 14:54:11 +0600

    updated for webcam

---
+++ Open_CV/02-io_code/io_webcam.py
@@ -0,0 +1,16 @@
+import os
+import cv2
+
+#read webcam
+webcam=cv2.VideoCapture(2)
+
+#visulaize webcam
+while True:
+    ret,frame=webcam.read()
+
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(40) & 0xFF==ord('q'):
+        break
+
+webcam.release()
+cv2.destroyAllWindows()
